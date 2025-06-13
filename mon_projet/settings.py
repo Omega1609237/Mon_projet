1261d1b0fe27c554225697ceacb43aecc047626d
Omega1609237 <ecclesiasteomega1@gmail.com>
2025-06-13 02:38:19 +0100

    Ajout du Procfile et configuration pour Render

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ara(6unap&ocszl$x#6#kyc=4_mg_#k(r0dyd416v6_ee0jk+r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','127.0.0.1']
 
 
 # Application definition
